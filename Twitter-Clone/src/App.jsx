7e91a34149ac3b3d59dec5666f09a99e6fad92ee
MattiaNini <121028609+MattiaNini@users.noreply.github.com>
2023-03-07 16:27:19 +0100

    Twitter Clone Project
    Es: 07-03-2023
    
    - Aggiunta sezione SideMenu
    - Aggiunto Hamburger Menu

--- Twitter-Clone/src/App.jsx
+++ Twitter-Clone/src/App.jsx
@@ -1,10 +1,14 @@
 import Content from "./components/content";
+import SideTrends from "./components/sideTrends";
+import SideMenu from "./components/sideMenu";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <SideMenu />
       <Content />
+      <SideTrends />
     </div>
   );
 }
